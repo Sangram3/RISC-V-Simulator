a3c8bb2e26caa48f9d577f74b23235329559c600
nupur-rathi <67869502+nupur-rathi@users.noreply.github.com>
2021-03-28 16:00:41 +0530

    Update decode.py

--- decode.py
+++ decode.py
@@ -1,1 +1,35 @@
 #decode
+#dic having all types of instructions with their pneumonics, FMT, opcodes, func3 and func7 (map)
+
+#get IR
+
+#all the below classes have getter method
+
+#OPcode class
+#-extract opcode method
+
+#func3 class
+#-extract func3 method
+
+#func7 class
+#-extract func7 method
+
+#pneumonic class
+#-decode pneumonic
+
+#fMT class
+#-decode fmt 
+
+#rs1 class
+#-extract rs1
+
+#rs2 class
+#-extract rs2
+
+#rd class
+#-extract rd
+
+#imm class
+#-extract imm
+
+#driver func used to call all the above methods accordingly when needed.
