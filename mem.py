7ea0951e3d6332a23db762a794d02b408b8d1949
Sangram3 <58004845+Sangram3@users.noreply.github.com>
2021-03-28 13:57:09 +0530

    Update mem.py

--- mem.py
+++ mem.py
@@ -5,10 +5,11 @@
 #dictionary <int,int> ~ address,value>
 
 #create following class variables:
-#code_start
-#data_start
+#code_start ~ text segment
+#data_start ~ data segment
 #data_stop 
-#stack_start
+#stack_start ~ stack segment
+#heap_start ~ heap segment
 
 #create following methods/functions
 #load_data,load_word,load_byte etc.
