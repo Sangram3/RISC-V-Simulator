e193b62e90b04db5e714e68959aa4e3102889883
Sangram3 <58004845+Sangram3@users.noreply.github.com>
2021-03-28 13:53:37 +0530

    Update mem.py

--- mem.py
+++ mem.py
@@ -1,1 +1,19 @@
 #memory access
+
+#create memory class
+#create dictionary~CPU Memory
+#dictionary <int,int> ~ address,value>
+
+#create following class variables:
+#code_start
+#data_start
+#data_stop 
+#stack_start
+
+#create following methods/functions
+#load_data,load_word,load_byte etc.
+#store_data,store_word,store_byte etc.
+
+#create PC class
+#create Registers class
+#create ... 
