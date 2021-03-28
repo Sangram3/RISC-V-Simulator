e732f9aaec28bb50f4f20fb7e60e3b9c5caf32b4
nupur-rathi <67869502+nupur-rathi@users.noreply.github.com>
2021-03-28 15:04:16 +0530

    Update fetch.py

--- fetch.py
+++ fetch.py
@@ -1,1 +1,8 @@
 #fetch
+#create IR class with following:-
+
+#-IR variable
+#-set IR method (fetch step)(sets current instruction according to PC value)
+#-get IR method (returns current instruction)
+
+#method to increment PC (PC+4) by calling add_PC in PC class
