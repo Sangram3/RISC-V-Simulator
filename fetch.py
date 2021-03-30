377b512d5418bac3baa592a4d484398ca9eb3e66
Sangram3 <58004845+Sangram3@users.noreply.github.com>
2021-03-30 21:07:32 +0530

    Update fetch.py

--- fetch.py
+++ fetch.py
@@ -6,3 +6,35 @@
 #-get IR method (returns current instruction)
 
 #method to increment PC (PC+4) by calling add_PC in PC class
+
+#code starts here
+from collections import defaultdict
+class IR():
+    f = "file is empty" # .mc file will be opened in this variable
+    inst_reg = defaultdict() #this will contain all the instructions along with the addresses
+
+    def __init__(self): #constructor
+        pass
+    
+    def open_file(self,filename):# opens .mc file and updates Instruction register
+        self.f = open(filename) 
+        for p in self.f:
+            p = p.strip()
+            if len(p)>0:
+                if p[0]!= '.':
+                    p = p.split()
+                    IR.inst_reg.update({p[0]: p[1]})
+                    
+    def print_IR(self): #print Instruction Register
+        for key in self.inst_reg:
+            print(key,self.inst_reg[key])
+            
+    def get_instruction(self): #gets next instruction
+        pass
+        
+    def Set_IR(self): #sets PC value
+        pass
+        
+inst_register = IR()
+inst_register.open_file("add.mc")
+inst_register.print_IR()
