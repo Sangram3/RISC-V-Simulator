d9c8c90bfbefdca6fd68dd2b88226753917e8597
Antara-Ag <81512219+Antara-Ag@users.noreply.github.com>
2021-04-04 21:17:12 +0530

    Update mem.py

--- mem.py
+++ mem.py
@@ -1,5 +1,6 @@
-def write_back(write_control, reg_no, data):      #reg_no is found using reg_obj.get_rd() function, data is in hex (with 0x)
-    if(write_control):                            
+def write_back(write_control, reg_obj, data):      #reg_no is found using reg_obj.get_rd() function, data is in hex (with 0x)
+    if(write_control):    
+        reg_no = reg_obj.get_rd()
         reg_obj.store_reg(reg_no, data)               #reg_obj is the object of the registers class
 
 # def memory_access(memory_control, address, reg_no, data):
