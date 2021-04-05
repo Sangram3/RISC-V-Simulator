6d0ad03c353483bafb080da1f6554b9c05273320
nupur-rathi <67869502+nupur-rathi@users.noreply.github.com>
2021-04-05 23:11:29 +0530

    Update RISCV-Sim.py

--- RISCV-Sim.py
+++ RISCV-Sim.py
@@ -4,26 +4,39 @@ from fetch import *
 from decode import *
 from execute import *
 from mem import *
+from control import *
+from write_back import *
 
 mc_file = "test.mc"
 
 mem_mod = memory(mc_file)
 reg_mod = registers()
+clock = 0
 
 while(1):
     fetch(mem_mod, reg_mod)
-
+    if(reg_mod.get_IR() == '0xEF000011'):
+        mem_mod.code_ends('output.mc')
+        break
     return_of_decode = list(decode(bin32(int(reg_mod.get_IR(),16)) , mem_mod, reg_mod))
+    control_bits = control(return_of_decode)
     return_of_execute = execute(return_of_decode[0], return_of_decode[1], return_of_decode[2],reg_mod)
-    write_back(1, reg_mod, return_of_execute)
+    # return_of_mem = mem(control_bits[1], mem_obj, return_of_execute, data)
+    if(control_bits[1] != 0):
+        return_of_mem = mem(control_bits[1], mem_mod, reg_mod, return_of_execute)
+
+    if(return_of_decode[1] == 'lw' or return_of_decode[1] == 'lh' or return_of_decode[1] == 'lb'):
+        write_back(control_bits[0], reg_mod, return_of_mem)
+
+    else:
+        write_back(control_bits[0], reg_mod, return_of_execute)
+    clock = clock+1
     
-
-
 # print(return_of_execute)
 
     
-
-# reg_mod.print_reg()
+mem_mod.print_mem()
+reg_mod.print_reg()
 # print(decode(bin32(reg_mod.get_IR()[2:]), mem_mod, reg_mod ))
 # print(fetch(mem_mod,reg_mod))
 # print(reg_mod.get_IR())
