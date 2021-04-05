cfae32f0f3e43ad1702fb5e39ade925596c24abc
nupur-rathi <67869502+nupur-rathi@users.noreply.github.com>
2021-04-05 23:13:58 +0530

    Update mem.py

--- mem.py
+++ mem.py
@@ -1,30 +1,51 @@
-  
-def write_back(write_control, reg_obj, data):      #reg_no is found using reg_obj.get_rd() function, data is in hex (with 0x)
-    if(write_control):   
-        reg_no = reg_obj.get_rd()                         
-        reg_obj.store_reg(reg_no, data) 
+def dec_to_2s(data, digits):
+    return (hex(2**(digits*4) - data))
+
+def Twos_to_dec(hex):
+    return -(2**((len(hex)-2)*4) - int(hex, 16))
 
 
-def mem(if_needed, mem_control, mem_obj, add, *args):   #data in hex with 0x
-    if(if_needed):
-        if(mem_control == 1):
-            # sw
-            sw(add, args[0])
-        else if(mem_control == 2):
-            # sh
-            sh(add,args[0])
-        else if(mem_control == 3):
-            # sb
-            sb(add,args[0])   #args[0] is the data in hexwith 0x
-        else if(mem_control == 4):
-            # lw    
-            lw(add)
-        else if(mem_control == 5):
-            # lb
-            lb(add)
-        else if(mem_control == 6):
-            lh(add)
-            # lh    #     #      
+
+def mem(mem_control, mem_obj, reg_obj, add):   #data in hex with 0x 
+    x = 0
+    if(mem_control == 1):
+        # sw
+        mem_obj.sw(add, hex(reg_obj.load_reg(reg_obj.get_rs2())))
+    elif(mem_control == 2):
+        # sh
+        mem_obj.sh(add, hex(reg_obj.load_reg(reg_obj.get_rs2())))
+    elif(mem_control == 3):
+        # sb
+        mem_obj.sb(add, hex(reg_obj.load_reg(reg_obj.get_rs2())))   #args[0] is the data in hexwith 0x
+    elif(mem_control == 4):
+        # lw    
+        x = mem_obj.lw(add)
+        print(x)
+        if(int(x[2],16) > 7):
+            x = Twos_to_dec(x)
+            print (x)
+        else:
+            x = int(x, 16)
+    elif(mem_control == 5):
+        # lh
+        
+        x = mem_obj.lh(add)
+        print(x)
+        if(int(x[2],16) > 7):
+            x = Twos_to_dec(x)
+            print (x)
+        else:
+            x = int(x, 16)
+    elif(mem_control == 6):
+        x = mem_obj.lb(add)
+        print(x)
+        if(int(x[2],16) > 7):
+            x = Twos_to_dec(x)
+            print (x)
+        else:
+            x = int(x, 16)  
+    return x
+
 
 
 
