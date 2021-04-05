119e6ab6b086b623ac515842c2d3a96d9e20dbc6
Bharat-Sharma-afk <79968768+Bharat-Sharma-afk@users.noreply.github.com>
2021-04-05 11:30:08 +0530

    Update gui.py
    Added Browse file Option and edited the registers and memory tab

--- gui.py
+++ gui.py
@@ -19,9 +19,30 @@ class Window(QWidget):
    def EditorTabUI(self):
       editorTab = QWidget()
       layout = QVBoxLayout()
+      
+      self.fn='File'
+      filebox = QHBoxLayout()
+      self.f=QLabel("File")
+      filebox.addWidget(self.f)
+      filebox.addStretch()
+
+      btn = QPushButton("Select File")
+      filebox.addWidget(btn)
+      filebox.addStretch()
+
+      btn.clicked.connect(self.getfile)
+      
+      layout.addLayout(filebox)
       layout.addWidget(QTextEdit())
+      
       editorTab.setLayout(layout)
       return editorTab
+
+   def getfile(self):
+      fname = QFileDialog.getOpenFileName(self, 'Open file', 'D:\\4th_Sem\CS204\Project',"Machine Cdde files (*.mc)")
+      print(str(fname[0]))
+      self.f.setText(str(fname[0]))
+   
 
    def CompilerTabUI(self):
       compilerTab = QWidget()
@@ -30,28 +51,52 @@ class Window(QWidget):
 
       run= QHBoxLayout()
       run.addWidget(QPushButton("Run"))
+      run.addStretch()
       run.addWidget(QPushButton("Step"))
+      run.addStretch()
       run.addWidget(QPushButton("Reset"))
+      run.addStretch()
       run.addWidget(QPushButton("Dump"))
-      
+      run.addStretch()
+
       lef_s.addLayout(run)
-      for i in range(20):
+      layout.addStretch()
+
+      for i in range(5):
          lef_s.addWidget(QLabel("Inst"))  
+         lef_s.addStretch()
+      self.Output= QLabel("Output")
+      lef_s.addWidget(self.Output)
+      lef_s.addStretch()
       
       layout.addLayout(lef_s)
-
+      layout.addStretch()
+      
       tabs = QTabWidget()
       tabs.addTab(self.MemoryTabUI(), "Memory")
       tabs.addTab(self.RegisterTabUI(), "Register")
       layout.addWidget(tabs)
+      layout.addStretch()
 
       compilerTab.setLayout(layout)
       return compilerTab
 
    def MemoryTabUI(self):
       memoryTab = QWidget()
-      layout = QVBoxLayout()
-      layout.addWidget(QLabel("Memory"))
+      layout = QGridLayout()
+      layout.addWidget(QLabel("Address"),0,0)
+      layout.addWidget(QLabel("+0"),0,1)
+      layout.addWidget(QLabel("+1"),0,2)
+      layout.addWidget(QLabel("+2"),0,3)
+      layout.addWidget(QLabel("+4"),0,4)
+
+      for i in range(10):
+         for j in range(5):
+            if(j==0):
+               layout.addWidget(QLabel("0x0"),i+1,j) 
+            else:
+               layout.addWidget(QLabel("00"),i+1,j)
+      
       memoryTab.setLayout(layout)
       return memoryTab
 
@@ -59,7 +104,14 @@ class Window(QWidget):
 
       registerTab = QWidget()
       layout = QVBoxLayout()
-      layout.addWidget(QLabel("Registers"))
+
+      
+      for i in range(32):
+         reg = QHBoxLayout()
+         reg.addWidget(QLabel("x"+str(i+1)))
+         reg.addWidget(QLabel("0x0"))
+         layout.addLayout(reg)
+
       registerTab.setLayout(layout)
       return registerTab
 
