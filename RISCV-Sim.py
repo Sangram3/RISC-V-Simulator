e3f32dd0f86ef94274ed140e2bc51fff04177e79
nupur-rathi <67869502+nupur-rathi@users.noreply.github.com>
2021-04-06 20:48:46 +0530

    Update RISCV-Sim.py

--- RISCV-Sim.py
+++ RISCV-Sim.py
@@ -7,6 +7,35 @@ from mem import *
 from control import *
 from write_back import *
 
+def bin_to_dec(s): # input in two's compliment form
+    if s[0] == '1':
+        return -1 * (int(''.join('1' if x == '0' else '0' for x in s), 2) + 1)
+    else:
+        return int(s, 2)
+
+def basic_code(dec_out, reg, mem):
+    code = ""
+    if(dec_out[0] == 1): #R
+        code = dec_out[1]+ " x" + str(reg.get_rd()) + " x" + str(reg.get_rs1()) + " x"+ str(reg.get_rs2())
+    elif(dec_out[0] == 2):
+        imm = bin_to_dec(dec_out[2])
+        if(dec_out[1] == 'lw' or dec_out[1] == 'lb' or dec_out[1] == 'lh'):
+            code = dec_out[1] + " x" + str(reg.get_rd()) + " " + str(imm)+"(x" +str(reg.get_rs1())+")"
+        else:    
+            code = dec_out[1] + " x" + str(reg.get_rd()) + " x" + str(reg.get_rs1()) + " " +str(imm)
+    elif(dec_out[0] == 3):
+        imm = bin_to_dec(dec_out[2])
+        code = dec_out[1] + " x" + str(reg.get_rs2()) + " " + str(imm)+"(x" +str(reg.get_rs1())+")"
+    elif(dec_out[0] == 4):
+        imm = bin_to_dec(dec_out[2])
+        code = dec_out[1] + " x" + str(reg.get_rs1()) + " x" + str(reg.get_rs2()) + " " +str(imm)
+    elif(dec_out[0] == 5 or dec_out[0] == 6):
+        imm = bin_to_dec(dec_out[2])
+        code = dec_out[1] + " x" + str(reg.get_rd()) +" " +str(imm)
+    else:
+        code = "Wrong Instruction"
+    return code
+
 def main():
 
     dic = {}
@@ -16,6 +45,9 @@ def main():
     mem_mod = memory(mc_file)
     reg_mod = registers()
     clock = 0
+    regi = [reg_mod.load_reg(i) for i in range(32)]
+    memi = mem_mod.get_mem()
+    dic[clock] = [regi, memi, reg_mod.get_PC(), reg_mod.get_IR()]
             
     while(1):
         fetch(mem_mod,  reg_mod)
@@ -37,9 +69,14 @@ def main():
         clock =  clock+1
         
         regi = [reg_mod.load_reg(i) for i in range(32)]
-        memi = mem_mod.dic()
-        dic[clock] = [regi, memi, reg_mod.get_PC(), reg_mod.get_IR()]
-        print(dic)
+        memi = mem_mod.get_mem()
+        code = basic_code(return_of_decode, reg_mod, mem_mod)
+        print(code)
+        dic[clock] = [regi, memi, reg_mod.get_PC(), reg_mod.get_IR(), code]
+        #print(dic)
+        print(reg_mod.get_PC())
+        mem_mod.print_mem()
+        reg_mod.print_reg()
     
     return dic
 
