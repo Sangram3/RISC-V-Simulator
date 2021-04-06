6811bda9b5d4dc9aa910cf4133ea2bcd3e11fd09
Bharat-Sharma-afk <79968768+Bharat-Sharma-afk@users.noreply.github.com>
2021-04-06 16:56:08 +0530

    Update gui.py
    Mem Reg Pane Update

--- gui.py
+++ gui.py
@@ -113,8 +113,10 @@ class Window(QtWidgets.QTabWidget):
       self.fn='File'
       filebox = QHBoxLayout()
       self.f=QLabel("File")
+
+      filebox.addStretch(0)
       filebox.addWidget(self.f)
-      filebox.addStretch()
+      filebox.addStretch(0)
 
       btn = QPushButton("Select File")
       btn.setStyleSheet("QPushButton"
@@ -127,7 +129,7 @@ class Window(QtWidgets.QTabWidget):
                              "}"
                              )
       filebox.addWidget(btn)
-      # filebox.addStretch()
+      filebox.addStretch(2)
 
       btn.clicked.connect(self.getfile)
       
@@ -202,10 +204,13 @@ class Window(QtWidgets.QTabWidget):
       scroll = QScrollArea()
       scroll.setWidget(groupBox)
       scroll.setWidgetResizable(True)
-      scroll.setFixedHeight(300)
+      scroll.setFixedHeight(500)
       scroll.setFixedWidth(1000)
 
       lef_s.addWidget(scroll)
+      
+      lef_s.addWidget(QLabel())
+      lef_s.addWidget(QLabel())
 
       gB = QGroupBox("Output")
       vb= QFormLayout()
@@ -262,7 +267,7 @@ class Window(QtWidgets.QTabWidget):
    def dump_code(self):
        # dump()
        print("dump clicked")
-       
+        
    def MemoryTabUI(self):
       memoryTab = QWidget()
       f_lay = QVBoxLayout()
@@ -275,26 +280,38 @@ class Window(QtWidgets.QTabWidget):
          t = []
          for j in range(5):
             if(j==0):
-               t.append(QLabel(hex(4*i))) 
+               t.append(QLabel((hex(4*i).upper()+"    "))) 
             else:
                t.append(QLabel("00"))
                
          self.labels.append(t)
       
+      add_l=QLabel("Address     ")
+      add_l.setAlignment(Qt.AlignCenter)
+      plus0 = QLabel(" +0 ")
+      plus0.setAlignment(Qt.AlignCenter)
+      plus1 = QLabel(" +1 ")
+      plus1.setAlignment(Qt.AlignCenter)
+      plus2 = QLabel(" +2 ")
+      plus2.setAlignment(Qt.AlignCenter)
+      plus3 = QLabel(" +4 ")
+      plus3.setAlignment(Qt.AlignCenter)
       
-      layout.addWidget(QLabel("Address"),0,0,1,1)
-      layout.addWidget(QLabel("+0"),0,1,1,2)
-      layout.addWidget(QLabel("+1"),0,2,1,2)
-      layout.addWidget(QLabel("+2"),0,3,1,2)
-      layout.addWidget(QLabel("+4"),0,4,1,2)
+      layout.addWidget(add_l, 0,0)
+      layout.addWidget(plus0, 0,1)
+      layout.addWidget(plus1, 0,2)
+      layout.addWidget(plus2, 0,3)
+      layout.addWidget(plus3, 0,4)
       
 
       for i in range(10):
          for j in range(5):
-            if(j==0):
-               layout.addWidget(self.labels[i][j],i+1,j,1,1)   
-            layout.addWidget(self.labels[i][j],i+1,j,1,2)
+            self.labels[i][j].setAlignment(Qt.AlignCenter)
+            if(j!=0):
+               self.labels[i][j].setStyleSheet("border: 1px solid white;")   
+            layout.addWidget(self.labels[i][j],i+1,j)
       
+      #f_lay.addStretch(0)
       f_lay.addLayout(layout)
       
       nav_pane = QHBoxLayout()
@@ -314,8 +331,10 @@ class Window(QtWidgets.QTabWidget):
       nav_pane.addWidget(self.jump)
       nav_pane.addWidget(up)
       nav_pane.addWidget(down) 
+      
+      #f_lay.addStretch(0)
       f_lay.addLayout(nav_pane)
-      f_lay.addStretch()
+      
 
       jumpto = QHBoxLayout()
       jumpto.addWidget(QLabel("Jump to"))
@@ -325,7 +344,9 @@ class Window(QtWidgets.QTabWidget):
       jump_add.textChanged.connect(self.textChanged)
       jump_add.editingFinished.connect(self.jump_enterPress)
 
+      #f_lay.addStretch(0)
       f_lay.addLayout(jumpto)
+      #f_lay.addStretch(0)
 
       memoryTab.setLayout(f_lay)
       return memoryTab
@@ -358,10 +379,10 @@ class Window(QtWidgets.QTabWidget):
       for i in range(10):
          for j in range(5):
             if(j==0):
-               self.labels[i][j].setText(hex(4*i+(40*self.mem_page)))
+               self.labels[i][j].setText(hex(4*i+(40*self.mem_page)).upper()+"    ")
             else:
                if(4*i+(40*self.mem_page)+j-1 in self.memory):
-                  self.labels[i][j].setText(self.memory[4*i+(40*self.mem_page)+j-1])
+                  self.labels[i][j].setText(self.memory[4*i+(40*self.mem_page)+j-1].upper())
                elif(4*i+(40*self.mem_page)+j-1 < 0 or 4*i+(40*self.mem_page)+j-1> 2147483647):
                   self.labels[i][j].setText("--")
                else:
@@ -374,11 +395,22 @@ class Window(QtWidgets.QTabWidget):
 
       gb = QGroupBox()
 
+      self.reg_labels = [QLabel("0x00000000") for i in range(32)]
+      
       layout = QVBoxLayout()
+
       for i in range(32):
          reg = QHBoxLayout()
-         reg.addWidget(QLabel("x"+str(i+1)))
-         reg.addWidget(QLabel("0x0"))
+         reg_name = QLabel("x"+str(i))
+         reg_name.setMaximumSize(100, 50)
+         reg_name.setAlignment(Qt.AlignCenter)
+         reg_value = self.reg_labels[i]
+         reg_value.setAlignment(Qt.AlignCenter)
+         reg_value.setStyleSheet("border: 1px solid white;")   
+         
+         reg.addWidget(reg_name)
+         reg.addWidget(reg_value)
+
          layout.addLayout(reg)
 
       gb.setLayout(layout)
@@ -386,13 +418,33 @@ class Window(QtWidgets.QTabWidget):
       
       scr.setWidget(gb)
       scr.setWidgetResizable(True)
-      scr.setFixedHeight(700)
-      scr.setFixedWidth(200)
+      scr.setFixedHeight(800)
+      scr.setFixedWidth(400)
 
       v.addWidget(scr)
       registerTab.setLayout(v)
       return registerTab
 
+   def reg_pane_update(self):
+      for i in range(32):
+         val = ''
+         if(self.register[i]<0): 
+            val = self.dec_to_2s(self.register[i],32)
+         else:
+            val = self.extend_hex(self.register[i],32)
+         self.reg_labels[i].setText(val)
+
+   def dec_to_2s(data, digits):
+      return (hex(2**(digits*4) - data))
+   
+   def extend_hex(data, digits):
+      ans =hex(data)[2:]
+      if(len(hex(data)) < 10):
+         for i in range(10- len(hex(data))):
+            ans = "0"+ans
+         return "0x" + ans
+      else:
+         return hex(data)
 if __name__ == "__main__":
    app = QApplication(sys.argv)
    app.setStyleSheet("QLabel{font-size: 12pt;font-color: white;}")
