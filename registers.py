145d8d729bd33fa325489a52844c35bba14ced2c
Sangram3 <58004845+Sangram3@users.noreply.github.com>
2021-04-07 12:56:09 +0530

    Update registers.py

--- registers.py
+++ registers.py
@@ -8,7 +8,7 @@ class registers:
     __rs2 = 0
     __rd = 0
     clock = 0
-
+    
     def reset_regs(self):
         self.__regs=[0 for i in range(32)]
         self.__regs[2] =  int("0x7FFFFFFC",16) #stack pointer
@@ -19,19 +19,20 @@ class registers:
         self.__rs2 = 0
         self.__rd = 0
         self.clock = 0
-
+        
+    
     def get_clock(self):
         return self.clock
 
     def add_clock(self):
         self.clock = self.clock+1  
-
+    
     def get_regs(self):
         return self.__regs
-
+    
     def make0(self):
         self.__regs[0] = 0
- 
+    
     def print_reg(self):
         print(self.__regs)
 
