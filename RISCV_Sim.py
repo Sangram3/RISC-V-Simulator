4838878f70315e31417c466466a88746fc0921f2
Antara-Ag <81512219+Antara-Ag@users.noreply.github.com>
2021-04-08 12:16:22 +0530

    Update RISCV_Sim.py

--- RISCV_Sim.py
+++ RISCV_Sim.py
@@ -36,48 +36,6 @@ def basic_code(dec_out, reg, mem):
         code = "Wrong Instruction"
     return code
 
-# def main():
-
-#     dic = {}
-#     mc_file = "test.mc"
-#     mem_mod = memory(mc_file)
-#     reg_mod = registers()
-#     clock = 0
-#     regi = [reg_mod.load_reg(i) for i in range(32)]
-#     memi = mem_mod.get_mem()
-#     dic[clock] = [regi, memi, reg_mod.get_PC(), reg_mod.get_IR()]
-            
-#     while(1):
-#         fetch(mem_mod,  reg_mod)
-#         if(reg_mod.get_IR() == '0xEF000011'):
-#             mem_mod.code_ends('output.mc')
-#             break
-#         return_of_decode = list(decode(bin32(int( reg_mod.get_IR(),16)) ,  mem_mod,  reg_mod))
-#         control_bits = control(return_of_decode)
-#         return_of_execute = execute(return_of_decode[0], return_of_decode[1], return_of_decode[2], reg_mod)
-#         # return_of_mem = mem(control_bits[1], mem_obj, return_of_execute, data)
-#         if(control_bits[1] != 0):
-#             return_of_mem = mem(control_bits[1],  mem_mod,  reg_mod, return_of_execute)
-
-#         if(return_of_decode[1] == 'lw' or return_of_decode[1] == 'lh' or return_of_decode[1] == 'lb'):
-#             write_back(control_bits[0],  reg_mod, return_of_mem)
-
-#         else:
-#             write_back(control_bits[0],  reg_mod, return_of_execute)
-#         clock =  clock+1
-        
-#         regi = [reg_mod.load_reg(i) for i in range(32)]
-#         memi = mem_mod.get_mem()
-#         code = basic_code(return_of_decode, reg_mod, mem_mod)
-#         # print(code)
-#         dic[clock] = [regi, memi, reg_mod.get_PC(), reg_mod.get_IR(), code]
-#         # print(dic)
-#         # print(reg_mod.get_PC())
-#         mem_mod.print_mem()
-#         reg_mod.print_reg()
-    
-#     return dic
-
 mc_file = "temp.mc"
 mem_mod = memory(mc_file)
 reg_mod = registers()
