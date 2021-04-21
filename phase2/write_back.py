a4522ee5e37ce9453745bf8f1f30b66aa7da9f45
nupur-rathi <67869502+nupur-rathi@users.noreply.github.com>
2021-04-21 13:55:52 +0530

    Update write_back.py

--- phase2/write_back.py
+++ phase2/write_back.py
@@ -1,6 +1,6 @@
 def write_back(reg_obj, buffers):      # data is in hex (with 0x)
     
-    if(buffers[3].rd != -1):   
+    if(buffers[3].fmt == 1 or buffers[3].fmt == 2 or buffers[3].fmt == 5 or buffers[3].fmt == 6):   
         reg_no = buffers[3].rd 
         data = buffers[3].RY                        
         reg_obj.store_reg(reg_no, data)
