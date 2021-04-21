5cfb5ec04184cf1c55f3303a2a5e726df6b1a0d0
nupur-rathi <67869502+nupur-rathi@users.noreply.github.com>
2021-04-21 18:08:01 +0530

    Update data_forw.py

--- phase2/data_forw.py
+++ phase2/data_forw.py
@@ -1,4 +1,4 @@
-#  d = [EE:0, ME:0, MM:0, MES: 0, ED:0, MD:0, EDS:0, MDS:0]
+# d = {"EE": [0,0], "ME": [0,0], "MM": [0,0] "MES": [0,0], "ED": [0,0], "MD": [0,0], "EDS": [0,0], "MDS": [0,0]}
 # EE = 1, ME = 2, MM = 3, ED = 4, MD = 5, MES = 6, EDS = 7, MDS = 8
 # reg = which register acts as hazard, that is rs1 or rs2 or both # 1 for rs1, 2 for rs2, 3 for both
 # in below example both rs1 and rs2 acts as a hazard :
@@ -67,17 +67,3 @@ def data_forw(forw_type, reg, buffers):
 #         if(d[MM] == 1):
 #             data_forw(MM)
 #             d[MM] = 0
-
-# in decode step :
-#     decode()
-#     then we will check if dfata hazard and which type and update d accordingly
-#     then if data hazaerd and data_forwarding is to be done:
-#         if(d[ME] == 1):
-#             data_forw(ME)
-#             d[ME] = 0
-#         if(d[EE] == 1):
-#             data_forw(EE)
-#             d[EE] = 0
-#         if(d[MES] == 1):
-#             stall by one cycle accordingly
-#             d[MES] = 0
