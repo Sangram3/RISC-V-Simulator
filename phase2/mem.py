685a9ffb204c4e316cde8fc68724036b1296c700
nupur-rathi <67869502+nupur-rathi@users.noreply.github.com>
2021-04-23 20:21:26 +0530

    Update mem.py

--- phase2/mem.py
+++ phase2/mem.py
@@ -3,8 +3,18 @@ def Twos_to_dec(hex):
 
 def mem(mem_obj, reg_obj, buffers, index, pipeline_obj):   #data in hex with 0x 
 
-    buffers[3] = buffers[2]
-    print("mne:", buffers[3].mne, buffers[3].rd)
+    buffers[3].RZ = buffers[2].RZ   #Output of the ALU. It can be used in data forwarding.
+    buffers[3].RM = buffers[2].RM   #It has the value for the store ins. This is forwarded to MDR in stage 4(memory access).
+    buffers[3].RY = buffers[2].RY   #The value which is to be written in the writeback stage.
+    buffers[3].rs1 = buffers[2].rs1  
+    buffers[3].rs2 = buffers[2].rs2 
+    buffers[3].rd = buffers[2].rd
+    buffers[3].MAR = buffers[2].MAR
+    buffers[3].MDR = buffers[2].MDR
+    buffers[3].operand1 = buffers[2].operand1
+    buffers[3].operand2 = buffers[2].operand2
+    buffers[3].fmt = buffers[2].fmt
+    buffers[3].mne = buffers[2].mne
 
     x = 0
     if(buffers[2].mne == "sw"):
