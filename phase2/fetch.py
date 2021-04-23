2b66818cdffed84839073fb2e7756527ceb3f0fb
Sangram3 <58004845+Sangram3@users.noreply.github.com>
2021-04-23 12:39:10 +0530

    Update fetch.py

--- phase2/fetch.py
+++ phase2/fetch.py
@@ -5,8 +5,12 @@ def fetch(reg_mod, mem_mod, btb, buffers, index, pipeline_obj):
     
     PC = reg_mod.get_PC()
     inst = mem_mod.lw(PC)
-    reg_mod.add_PC(4)
-
+    # print(inst)
+    if pipeline_obj.disable_PC == 0:
+        reg_mod.add_PC(4)
+    else:
+        return
+    # print(inst,PC)
     if(inst == "0xEF000011"):
         pipeline_obj.finish = 1
         return
