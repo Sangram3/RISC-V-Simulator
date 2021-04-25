35633175c1fe77d4fc1a85ebd00ea7ea3ed25bb4
Sangram3 <58004845+Sangram3@users.noreply.github.com>
2021-04-25 22:42:39 +0530

    Update fetch.py

--- phase2/fetch.py
+++ phase2/fetch.py
@@ -5,13 +5,20 @@ def fetch(reg_mod, mem_mod, btb, buffers, index, pipeline_obj):
     
     PC = reg_mod.get_PC()
     inst = mem_mod.lw(PC)
-    
+    print()
+    print(PC,inst)
+    print()
     if pipeline_obj.disable_PC == 0:
         if btb.ifPresent(PC) == False:
-            
             reg_mod.add_PC(4)
         else:
-            reg_mod.update_PC(btb.table[str(PC)][1] )
+            if btb.prediction(PC) == True:
+                # print(PC,"friends")
+                reg_mod.update_PC(btb.getTarget(PC) )
+                # print(reg_mod.get_PC())
+            else:
+                reg_mod.add_PC(4)
+                
     else:
         return
     # print(inst,PC)
