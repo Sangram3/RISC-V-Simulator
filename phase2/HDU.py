44c68d8f865f6c6d0bb1b4a63eb8c97d7db0739f
nupur-rathi <67869502+nupur-rathi@users.noreply.github.com>
2021-04-25 21:56:47 +0530

    Update HDU.py

--- phase2/HDU.py
+++ phase2/HDU.py
@@ -15,25 +15,25 @@ def HDU(buffers, knob_forwarding, prevInsList, d):
 
         #M to M
         if(len(prevInsList) >=2 and (this[0] == 3 and (prevInsList[-2][0] == 1 or prevInsList[-2][0] == 2))): #store after Rtype/Itype/load/jalr  
-            if(this[3] == prevInsList[-2][4]): #rs2 matches
+            if(this[3] == prevInsList[-2][4] and this[3] != 0): #rs2 matches
                 # d["MM"] = [1,0,1]
                 d["MM"] = [1,2]
 
-                if(this[2] == prevInsList[-2][4] and (prevInsList[-2][1] == "lw" or prevInsList[-2][1] == "lb" or prevInsList[-2][1] == "lh")): #rs1 and rd match
+                if(this[2] != 0 and this[2] == prevInsList[-2][4] and (prevInsList[-2][1] == "lw" or prevInsList[-2][1] == "lb" or prevInsList[-2][1] == "lh")): #rs1 and rd match
                     # d["MES"] = [1,1,1]
                     d["MES"] = [1,3]
 
         #M to E
         if(len(prevInsList) >=3 and (this[0] == 1 or this[0] == 2 or this[0] == 3)): #R, I, S ins
-            if(this[2] == prevInsList[-3][4]):
+            if(this[2] == prevInsList[-3][4] and this[2] != 0):
                 # d["ME"] = [1,1,0]
                 d["ME"] = [1,1]
-                if(this[3] == prevInsList[-3][4]):
+                if(this[3] == prevInsList[-3][4] and this[3] != 0):
                     # d["ME"] = [1,1,1]
                     d["ME"] = [1,3]
 
 
-            elif(this[3] == prevInsList[-3][4]):
+            elif(this[3] == prevInsList[-3][4] and this[3] != 0):
                 # d["ME"] = [1,0,1]
                 d["ME"] = [1,2]
 
@@ -41,27 +41,27 @@ def HDU(buffers, knob_forwarding, prevInsList, d):
         #EE
         if(len(prevInsList) >=2 and (this[0] == 1 or this[0] == 2 or this[0] == 3)):
             # if(prevInsList[-2][1] != "lh" and prevInsList[-2][1] != "lw" and prevInsList[-2][1] != "lb"):
-                if(this[2] == prevInsList[-2][4]): #rs1 == rd
+                if(this[2] == prevInsList[-2][4] and this[2] != 0): #rs1 == rd
                     # d["EE"] = [1,1,0]
                     d["EE"] = [1,1]
-                    if(this[3] == prevInsList[-2][4] and this[0] !=3): #rs2 == rd
+                    if(this[3] != 0 and this[3] == prevInsList[-2][4] and this[0] !=3): #rs2 == rd
                         # d["EE"] = [1,1,1]
                         d["EE"] = [1,3]
-                elif(this[3] == prevInsList[-2][4] and this[0] !=3): #rs2 == rd
+                elif(this[3] != 0 and this[3] == prevInsList[-2][4] and this[0] !=3): #rs2 == rd
                         # d["EE"] = [1,0,1]  
                         d["EE"] = [1,2]  
 
 
         #MES 
         if(len(prevInsList) >=2 and (this[0] == 1 or this[0] == 2 or this[0]==3) and(prevInsList[-2][1] == 'lb' or prevInsList[-2][1] == 'lh' or prevInsList[-2][1] == 'lw')):
-            if(this[2] == prevInsList[-2][4]):
+            if(this[2] != 0 and this[2] == prevInsList[-2][4]):
                 # d["MES"] = [1,1,0]
                 d["MES"] = [1,1]
-                if(this[3] == prevInsList[-2][4]):
+                if(this[3] != 0 and this[3] == prevInsList[-2][4]):
                     # d["MES"] = [1,1,1]
                     d["MES"] = [1,3]
 
-            elif(this[3] == prevInsList[-2][4]):
+            elif(this[3] != 0 and this[3] == prevInsList[-2][4]):
                 # d["MES"] = [1,0,1]
                 d["MES"] = [1,2]
         
@@ -88,50 +88,50 @@ def check_control_hazard(buffers, prevInsList, d):
 
     #MD
     if(len(prevInsList) >= 3 and (prevInsList[-4][0] == 1 or prevInsList[-4][0] == 2)): #load or R or I type
-        if(prevInsList[-4][4] == this[2]): #rs1 same
+        if(this[2] != 0 and prevInsList[-4][4] == this[2]): #rs1 same
             d["MD"] = [1,1]
-            if(prevInsList[-4][4] == this[3]): #rs2 and rs1 same
+            if(this[3] != 0 and prevInsList[-4][4] == this[3]): #rs2 and rs1 same
                 d["MD"] = [1,3]
-        elif(prevInsList[-4][4] == this[3]): #rs2 same            
+        elif(this[3] != 0 and prevInsList[-4][4] == this[3]): #rs2 same            
             d["MD"] = [1,2]
 
     #ED
     if(len(prevInsList) >= 3 and (prevInsList[-3][0] == 1 or prevInsList[-3][0] == 2 and(prevInsList[-3][1] != "lw" and prevInsList[-3][1] != "lh" and prevInsList[-3][1] != "lb"))):
-        if(prevInsList[-3][4] == this[2]): #rs1 same
+        if(this[2] != 0 and prevInsList[-3][4] == this[2]): #rs1 same
             d["ED"] = [1,1]
-            if(prevInsList[-3][4] == this[3]): #rs2 and rs1 same
+            if(this[3] != 0 and prevInsList[-3][4] == this[3]): #rs2 and rs1 same
                 d["ED"] = [1,3]
-        elif(prevInsList[-3][4] == this[3]): #rs2 same            
+        elif(this[3] != 0 and prevInsList[-3][4] == this[3]): #rs2 same            
             d["ED"] = [1,2]
 
     #MDS
     if(len(prevInsList) >= 3 and (prevInsList[-3][1] == "lw" or prevInsList[-3][1] == "lh" or prevInsList[-3][1] == "lb")):
-        if(prevInsList[-3][4] == this[2]): #rs1 same
+        if(this[2] != 0 and prevInsList[-3][4] == this[2]): #rs1 same
             d["MDS"] = [1,1]
             if(d["MD"][0] ==1): #bec this would have already been written back
                 d["MD"] = [0,0]
-            if(prevInsList[-3][4] == this[3]): #rs2 same            
+            if(this[3] != 0 and prevInsList[-3][4] == this[3]): #rs2 same            
                 d["MDS"] = [1,3]
                 if(d["MD"][0] ==1): #bec this would have already been written back
                     d["MD"] = [0,0]
-        elif(prevInsList[-3][4] == this[3]): #rs2 same            
+        elif(this[3] != 0 and prevInsList[-3][4] == this[3]): #rs2 same            
             d["MDS"] = [1,2]
             if(d["MD"][0] ==1): #bec this would have already been written back
                 d["MD"] = [0,0]
 
     #EDS
     if(len(prevInsList) >= 2 and (prevInsList[-2][0] == 1 or  prevInsList[-2][0] == 2 and(prevInsList[-2][1] != "lw" and prevInsList[-2][1] != "lh" and prevInsList[-2][1] != "lb"))):
-        if(prevInsList[-2][4] == this[2]): #rs1 same
+        if(this[2] != 0 and prevInsList[-2][4] == this[2]): #rs1 same
             d["EDS"] = [1,1]
             if(d["ED"][0] == 1):
                 d["MD"] = d["ED"]
                 d["ED"] = [0,0] 
-            if(prevInsList[-2][4] == this[3]): #rs2 same            
+            if(this[3] != 0 and prevInsList[-2][4] == this[3]): #rs2 same            
                 d["EDS"] = [1,3]
                 if(d["ED"][0] == 1):
                     d["MD"] = d["ED"]
                     d["ED"] = [0,0]   
-        elif(prevInsList[-2][4] == this[3]): #rs2 same            
+        elif(this[3] != 0 and prevInsList[-2][4] == this[3]): #rs2 same            
             d["EDS"] = [1,2]
             if(d["ED"][0] == 1):
                 d["MD"] = d["ED"]
@@ -139,9 +139,9 @@ def check_control_hazard(buffers, prevInsList, d):
 
     #MDSS
     if(len(prevInsList) >= 2 and (prevInsList[-2][1] == "lw" or prevInsList[-2][1] == "lh" or prevInsList[-2][1] != "lb")):
-        if(prevInsList[-2][4] == this[2]): #rs1 same
+        if(this[2] != 0 and prevInsList[-2][4] == this[2]): #rs1 same
             d["MDSS"] = [2,1]
-            if(prevInsList[-2][4] == this[3]):
+            if(this[3] != 0 and prevInsList[-2][4] == this[3]):
                 d["MDSS"] = [2,3]
-        elif(prevInsList[-2][4] == this[3]):
+        elif(this[3] != 0 and prevInsList[-2][4] == this[3]):
             d["MDSS"] = [2,2]
