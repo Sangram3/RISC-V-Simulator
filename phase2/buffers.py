6c0e4af2ab6345a82b4c775550c47d312df3e268
Antara-Ag <81512219+Antara-Ag@users.noreply.github.com>
2021-04-26 11:47:20 +0530

    Update buffers.py

--- phase2/buffers.py
+++ phase2/buffers.py
@@ -37,25 +37,26 @@ class InterStateBuffer:
 
 class BTB():
 
-    table = {}
+    
+    btb = {}
     
     def newKey(self, PC, Target_add,is_jal_jalr):  #FNTBT satic predictor
         if is_jal_jalr == 1:
-            self.table[str(PC)] = [True,Target_add]
+            self.btb[str(PC)] = [True,Target_add]
             return
         if(Target_add>=PC):  #forward branch not taken  #Imm is >= 0
-            self.table[str(PC)] = [False,Target_add]
+            self.btb[str(PC)] = [False,Target_add]
         else:
-            self.table[str(PC)] = [True,Target_add]
+            self.btb[str(PC)] = [True,Target_add]
 
     def prediction(self, PC):
-        return self.table[str(PC)][0]
+        return self.btb[str(PC)][0]
 
     def getTarget(self, PC):
-        return self.table[str(PC)][1]
+        return self.btb[str(PC)][1]
 
     def ifPresent(self,PC):
-        if str(PC) in self.table:
+        if str(PC) in self.btb:
             return True
         return False      
 
