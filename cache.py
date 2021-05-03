8e970ab6bc7d9a6ca5292064eb5690a11436c1e2
Bharat-Sharma-afk <79968768+Bharat-Sharma-afk@users.noreply.github.com>
2021-05-03 22:57:05 +0530

    Create cache.py

---
+++ cache.py
@@ -0,0 +1,96 @@
+import math
+
+class cache:
+    ways = 0
+    block_size = 0
+    cache_size = 0
+    block_count = 0
+    set_count = 0
+
+    accesses = 0
+    hit = 0
+
+    bo_len = 0
+    index_len = 0
+    tag_len = 0
+
+    def __init__(self, cache_size, block_size, ways):
+        
+        self.cache_size = cache_size
+        self.ways = ways
+        self.block_count = int(cache_size/block_size)
+        self.set_count = int(self.block_count / ways)
+        
+        self.bo_len = math.ceil(math.log2(block_size))
+        self.index_len = math.ceil(math.log2(self.set_count))
+
+        self.cache_array = [[["tag", 0, "data"] for _ways in range(ways)] for _set in range(self.set_count)]
+        self.pref_array = [[0 for _ways in range(ways) ]  for _set in range(self.set_count)]
+        
+    
+    def decode_address(self, address):
+        bo = int(address, 16)%(2**self.bo_len)
+        index = (int(address, 16)%(2**(self.index_len+self.bo_len)) - bo) >> self.bo_len
+        tag = (int(address, 16) - int(address, 16)%(2**(self.index_len+self.bo_len))) >> (self.index_len+self.bo_len)
+        return tag, index, bo
+
+    def read(self, address):
+        self.accesses+=1
+        tag, index, bo = self.decode_address(address)
+        hit, way_no = self.hit_miss(tag, index)
+        if(hit):
+            self.hit+=1
+            return self.cache_array[index][way_no][2]    #BO would be used here
+
+        else:
+            #data = mem_mod.read(address)
+            self.write(address, "data")
+            return "data"
+
+    def hit_miss(self, tag, index):
+        _set = self.cache_array[index]
+        for i in range(len(_set)):
+            if(_set[i][0] == tag and _set[i][1] == 1):
+                return True, i
+            else:
+                return False, 0
+
+    def write(self, address, data):
+        tag, index, bo = self.decode_address(address)
+        for _way in range(len(self.cache_array[index])):
+            if(self.cache_array[index][_way][1] == 0):
+                self.cache_array[index][_way][0] = tag
+                self.cache_array[index][_way][1] = 1
+                self.cache_array[index][_way][2] = data
+                self.set_pref(index, _way)
+                #mem_mod.write(address, data)
+                return
+
+        _way = self.pref_array[index].index(min(self.pref_array[index]))
+        self.cache_array[index][_way][0] = tag
+        self.cache_array[index][_way][1] = 1
+        self.cache_array[index][_way][2] = data
+        self.set_pref(index, _way)
+        #mem_mod.write(address, data)
+        return
+
+    def set_pref(self, index, way_no):
+        for i in range(len(self.pref_array[index])):
+            if(i == way_no):
+                self.pref_array[index][i] = self.ways
+            else:
+                if(self.pref_array[index][i] != 0):
+                    self.pref_array[index][i]-=1
+
+    
+
+
+x = cache(1256, 64, 4)
+x.write("ABC", 56)
+x.write("1BC", 12)
+x.write("2Ba", 15)
+x.write("ABb", 18)
+x.write("fBa", 19)
+print(x.cache_array)
+print(x.pref_array)
+print(x.read("ABC"))
