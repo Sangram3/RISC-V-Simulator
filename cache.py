41322e678c3d7347cff55ad2c6b9d904a7d260db
nupur-rathi <67869502+nupur-rathi@users.noreply.github.com>
2021-05-05 22:46:25 +0530

    Update cache.py

--- cache.py
+++ cache.py
@@ -6,9 +6,11 @@ class cache:
     cache_size = 0
     block_count = 0
     set_count = 0
+    words_per_block = 0
 
     accesses = 0
     hit = 0
+    miss = 0
 
     bo_len = 0
     index_len = 0
@@ -17,14 +19,16 @@ class cache:
     def __init__(self, cache_size, block_size, ways):
         
         self.cache_size = cache_size
+        self.block_size = block_size
         self.ways = ways
         self.block_count = int(cache_size/block_size)
         self.set_count = int(self.block_count / ways)
+        self.words_per_block = int(block_size/4)
         
         self.bo_len = math.ceil(math.log2(block_size))
         self.index_len = math.ceil(math.log2(self.set_count))
 
-        self.cache_array = [[["tag", 0, "data"] for _ways in range(ways)] for _set in range(self.set_count)]
+        self.cache_array = [[["tag", 0, ["data" for x in range(self.words_per_block)]] for _ways in range(ways)] for _set in range(self.set_count)]
         self.pref_array = [[0 for _ways in range(ways) ]  for _set in range(self.set_count)]
         
     
@@ -34,28 +38,36 @@ class cache:
         tag = (int(address, 16) - int(address, 16)%(2**(self.index_len+self.bo_len))) >> (self.index_len+self.bo_len)
         return tag, index, bo
 
-    def read(self, address):
+    def read(self, address, mem_mod):
         self.accesses+=1
         tag, index, bo = self.decode_address(address)
+        print(tag, index, bo)
         hit, way_no = self.hit_miss(tag, index)
         if(hit):
             self.hit+=1
-            return self.cache_array[index][way_no][2]    #BO would be used here
+            return self.cache_array[index][way_no][2][int(bo/4)]    #BO would be used here
 
         else:
-            #data = mem_mod.read(address)
-            self.write(address, "data")
-            return "data"
+            self.miss+=1
+            data = []
+            for i in range(self.words_per_block):
+                print("address ", int(address,16))
+                temp = mem_mod.lw(int(address,16)+4*i)
+                data.append(temp)
+            # data = mem_mod.lw(int(address,16))
+            self.write(address, data, mem_mod) #data is an array of words
+            return data[int(bo/4)]
 
     def hit_miss(self, tag, index):
         _set = self.cache_array[index]
         for i in range(len(_set)):
             if(_set[i][0] == tag and _set[i][1] == 1):
                 return True, i
-            else:
-                return False, 0
+            
+        return False, 0
 
-    def write(self, address, data):
+    def write(self, address, data, mem_mod):
+        self.accesses+=1
         tag, index, bo = self.decode_address(address)
         for _way in range(len(self.cache_array[index])):
             if(self.cache_array[index][_way][1] == 0):
@@ -63,7 +75,9 @@ class cache:
                 self.cache_array[index][_way][1] = 1
                 self.cache_array[index][_way][2] = data
                 self.set_pref(index, _way)
-                #mem_mod.write(address, data)
+                for i in range(self.words_per_block):
+                    mem_mod.sw(int(address,16)+4*i, data[i])
+                # mem_mod.sw(int(address,16), data)
                 return
 
         _way = self.pref_array[index].index(min(self.pref_array[index]))
@@ -71,7 +85,8 @@ class cache:
         self.cache_array[index][_way][1] = 1
         self.cache_array[index][_way][2] = data
         self.set_pref(index, _way)
-        #mem_mod.write(address, data)
+        for i in range(self.words_per_block):
+            mem_mod.sw(int(address,16)+4*i, data[i])
         return
 
     def set_pref(self, index, way_no):
@@ -82,15 +97,18 @@ class cache:
                 if(self.pref_array[index][i] != 0):
                     self.pref_array[index][i]-=1
 
+    def print_cache(self):
+        for i in range(self.set_count):
+            print(self.cache_array[i])
     
 
 
-x = cache(1256, 64, 4)
-x.write("ABC", 56)
-x.write("1BC", 12)
-x.write("2Ba", 15)
-x.write("ABb", 18)
-x.write("fBa", 19)
-print(x.cache_array)
-print(x.pref_array)
-print(x.read("ABC"))
+# x = cache(1256, 64, 4)
+# x.write("ABC", 56)
+# x.write("1BC", 12)
+# x.write("2Ba", 15)
+# x.write("ABb", 18)
+# x.write("fBa", 19)
+# print(x.cache_array)
+# print(x.pref_array)
+# print(x.read("ABC"))
