3eb678e2183dfa29cd2b15a6cf91ac59d8f29a45
nupur-rathi <67869502+nupur-rathi@users.noreply.github.com>
2021-05-06 17:33:24 +0530

    Update fetch.py

--- phase3/fetch.py
+++ phase3/fetch.py
@@ -7,10 +7,11 @@ def fetch(mem_mod, reg_mod, l, cache_ob, gui_util_obj):
     copy_set = cache_ob.cache_array[set_no][:]
 
     temp = ["Fetch", hex(PC), set_no, copy_set]
+    gui_util_obj.task2.append(temp)
     tag = None
     set_no = None
     bo = None
-    gui_util_obj.task2.append(temp)
+
     temp = []
 
     reg_mod.set_IR(inst)
