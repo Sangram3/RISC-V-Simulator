c47d13f1920dad3fd408726ea755222e757ed746
nupur-rathi <67869502+nupur-rathi@users.noreply.github.com>
2021-05-06 13:39:23 +0530

    Update mem_p.py

--- phase3/mem_p.py
+++ phase3/mem_p.py
@@ -1,7 +1,7 @@
 def Twos_to_dec(hex):
     return -(2**((len(hex)-2)*4) - int(hex, 16))
 
-def mem_p(mem_obj, reg_obj, buffers, index, pipeline_obj, cache_ob):   #data in hex with 0x 
+def mem_p(mem_obj, reg_obj, buffers, index, pipeline_obj, cache_ob, gui_util_obj):   #data in hex with 0x 
 
     buffers[3].RB = buffers[3].RY
 
@@ -21,27 +21,70 @@ def mem_p(mem_obj, reg_obj, buffers, index, pipeline_obj, cache_ob):   #data in 
     x = 0
     if(buffers[2].mne == "sw"):
         # sw
-        #cache_ob.write(hex(buffers[2].MAR), hex(buffers[2].RM), mem_obj)
         cache_ob.storedata(hex(buffers[2].MAR), hex(buffers[2].RM))
-        mem_obj.sw(buffers[2].MAR, hex(buffers[2].RM))  
+        mem_obj.sw(buffers[2].MAR, hex(buffers[2].RM))
+
+
+        tag, set_no, bo = cache_ob.decode_address(hex(buffers[2].MAR))
+        h, bno = cache_ob.hit_miss(tag, set_no)
+
+        if(h == True):
+            copy_set = cache_ob.cache_array[set_no][:]
+            temp = ["Store", hex(buffers[2].MAR), set_no, copy_set]
+            tag = None
+            set_no = None
+            bo = None
+            gui_util_obj.task2.append(temp)
+            temp = []
+         
 
     elif(buffers[2].mne == "sh"):
         # sh
-        #cache_ob.write(hex(buffers[2].MAR), hex(buffers[2].RM), mem_obj)
         cache_ob.storedata(hex(buffers[2].MAR), hex(buffers[2].RM))
         mem_obj.sh(buffers[2].MAR, hex(buffers[2].RM))
 
+        tag, set_no, bo = cache_ob.decode_address(hex(buffers[2].MAR))
+        h, bno = cache_ob.hit_miss(tag, set_no)
+
+        if(h == True):
+            copy_set = cache_ob.cache_array[set_no][:]
+            temp = ["Store", hex(buffers[2].MAR), set_no, copy_set]
+            tag = None
+            set_no = None
+            bo = None
+            gui_util_obj.task2.append(temp)
+            temp = []
+
     elif(buffers[2].mne == "sb"):
         # sb
-        #cache_ob.write(hex(buffers[2].MAR), hex(buffers[2].RM), mem_obj)
         cache_ob.storedata(hex(buffers[2].MAR), hex(buffers[2].RM))
         mem_obj.sb(buffers[2].MAR, hex(buffers[2].RM))
 
+        tag, set_no, bo = cache_ob.decode_address(hex(buffers[2].MAR))
+        h, bno = cache_ob.hit_miss(tag, set_no)
+
+        if(h == True):
+            copy_set = cache_ob.cache_array[set_no][:]
+            temp = ["Store", hex(buffers[2].MAR), set_no, copy_set]
+            tag = None
+            set_no = None
+            bo = None
+            gui_util_obj.task2.append(temp)
+            temp = []
+
     elif(buffers[2].mne == "lw"):
         # lw  
-        x = cache_ob.read(hex(buffers[2].MAR), mem_obj)
+        x = cache_ob.read(hex(buffers[2].MAR), mem_obj, "Load", gui_util_obj)
         
-        #x = mem_obj.lw(buffers[2].MAR)
+        tag, set_no, bo = cache_ob.decode_address(hex(buffers[2].MAR))
+        copy_set = cache_ob.cache_array[set_no][:]
+        temp = ["Load", hex(buffers[2].MAR), set_no, copy_set]
+        tag = None
+        set_no = None
+        bo = None
+        gui_util_obj.task2.append(temp)
+        temp = []
+
         if(int(x[2],16) > 7):
             x = Twos_to_dec(x)
         else:
@@ -51,8 +94,16 @@ def mem_p(mem_obj, reg_obj, buffers, index, pipeline_obj, cache_ob):   #data in 
 
     elif(buffers[2].mne == "lh"):
         # lh
-        x = cache_ob.read(hex(buffers[2].MAR), mem_obj)
-        #x = mem_obj.lh(buffers[2].MAR)
+        x = cache_ob.read(hex(buffers[2].MAR), mem_obj, "Load", gui_util_obj)
+        tag, set_no, bo = cache_ob.decode_address(hex(buffers[2].MAR))
+        copy_set = cache_ob.cache_array[set_no][:]
+        temp = ["Load", hex(buffers[2].MAR), set_no, copy_set]
+        tag = None
+        set_no = None
+        bo = None
+        gui_util_obj.task2.append(temp)
+        temp = []
+
         if(int(x[2],16) > 7):
             x = Twos_to_dec(x)
         else:
@@ -62,8 +113,17 @@ def mem_p(mem_obj, reg_obj, buffers, index, pipeline_obj, cache_ob):   #data in 
 
     elif(buffers[2].mne == "lb"):
         #lb
-        x = cache_ob.read(hex(buffers[2].MAR), mem_obj)
-        #x = mem_obj.lb(buffers[2].MAR)
+        x = cache_ob.read(hex(buffers[2].MAR), mem_obj, "Load", gui_util_obj)
+        
+        tag, set_no, bo = cache_ob.decode_address(hex(buffers[2].MAR))
+        copy_set = cache_ob.cache_array[set_no][:]
+        temp = ["Load", hex(buffers[2].MAR), set_no, copy_set]
+        tag = None
+        set_no = None
+        bo = None
+        gui_util_obj.task2.append(temp)
+        temp = []
+
         if(int(x[2],16) > 7):
             x = Twos_to_dec(x)
         else:
